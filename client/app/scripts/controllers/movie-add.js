343cb86c41bbac53cba49e7e090b08f0a7041557
Sanjay Sharma <sanjay@ekbana.com>
2015-08-04 10:38:28 +0545

    added CRUD routes and controllers for movies

---
+++ client/app/scripts/controllers/movie-add.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name clientApp.controller:MovieAddCtrl
+ * @description
+ * # MovieAddCtrl
+ * Controller of the clientApp
+ */
+angular.module('clientApp')
+  .controller('MovieAddCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ client/app/scripts/controllers/movie-view.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name clientApp.controller:MovieViewCtrl
+ * @description
+ * # MovieViewCtrl
+ * Controller of the clientApp
+ */
+angular.module('clientApp')
+  .controller('MovieViewCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
